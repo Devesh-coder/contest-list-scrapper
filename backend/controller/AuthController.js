7e88b33773057c724c7b9c68a4a364c78eecca6e
Devesh Agarwal <agarwaldevesh326@gmail.com>
2023-11-05 20:51:53 +0530

    1 day set cookie

--- backend/controller/AuthController.js
+++ backend/controller/AuthController.js
@@ -17,7 +17,7 @@ const callback = asyncHandler(async (req, res) => {
 	await updateUser(tokens.refresh_token, tokens.id_token)
 
 	// const maxAge = 6 * 30 * 24 * 60 * 60 * 1000
-	const maxAge = 1 * 60 * 1000 // 10 minute
+	const maxAge = 24 * 60 * 60 * 1000 // 1 day
 	// Ideally cookies should have the same expiry time as the jwt tokens
 	// res.cookie('refreshToken', 'actual token in cookie')
 
