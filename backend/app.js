9004dfb5b0bf52604809ffd0d63fe9ce381eb587
Devesh Agarwal <agarwaldevesh326@gmail.com>
2023-11-09 17:48:03 +0530

    configured based on aws

--- backend/app.js
+++ backend/app.js
@@ -35,6 +35,10 @@ app.use('/contests', contestsRoute)
 app.use('/create-event', calendarRoute)
 app.use('/auth', authRoute)
 
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
 })
--- backend/config/redis.js
+++ backend/config/redis.js
@@ -2,10 +2,15 @@ const redis = require('redis')
 const { createClient } = require('redis')
 
 const REDIS_PORT = process.env.PORT || 6379
+const REDIS_HOST = process.env.REDIS_HOST || 'localhost'
 
 // const redisClient = createClient(REDIS_PORT) // this creates a new client
 
-const redisClient = redis.createClient({ REDIS_PORT, legacyMode: true })
+const redisClient = redis.createClient({
+	REDIS_PORT,
+	REDIS_HOST,
+	legacyMode: true,
+})
 ;(async () => {
 	await redisClient.connect()
 })()
